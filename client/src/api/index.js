fca654cba868b0714b5f7cf06ba4884523675fb9
AbdullahGumi <abdulgumi77@gmail.com>
2023-01-15 16:27:38 +0100

    update api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://trelloboard.onrender.com"
+    ? "https://trelloboardservice.onrender.com"
     : "http://localhost:5000";
 
 const api = axios.create({
--- server/index.js
+++ server/index.js
@@ -13,9 +13,9 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/build/index.html"));
+// });
 
 app.use("/api/cards", cardsRouter);
 app.use("/api/listCards", listCardsRouter);
